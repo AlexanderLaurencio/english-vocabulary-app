e8f8ae41a3dff869bf794e96d57fd0fcc83522a8
Alexander <ramonalaurencio5@gmail.com>
2026-06-28 22:01:14 -0400

    thirds commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename="/english-vocabulary-app">
       <Routes>
-        <Route index element={<HomePage />}/>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
