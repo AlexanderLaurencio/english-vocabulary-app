0cb03ec7de7f069842718516a45c239fbe02bde0
Alexander <ramonalaurencio5@gmail.com>
2026-06-28 22:20:46 -0400

    fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from './pages/HomePage/HomePage.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />
+        }
+      ]
+    }
+  ],
+  {
+    basename: "/english-vocabulary-app"
+  }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/english-vocabulary-app">
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
